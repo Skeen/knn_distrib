6497537191b42d1cb7ccf0c66215052fbe43b31c
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-11-19 21:50:47 +0100

    Request in a loop, till we got a result!

--- request.sh
+++ request.sh
@@ -5,9 +5,8 @@
 
 SERVER_URL=http://localhost:3001
 
-TIMEOUT=10000
-SPLIT=1000
-KNN=1
+TIMEOUT=100000
+SPLIT=1
 
 REF_PATH=$1
 QUERY_PATH=$2
@@ -15,12 +14,16 @@ QUERY_PATH=$2
 NAME=$(curl -q \
   -F "timeout=$TIMEOUT" \
   -F "split=$SPLIT" \
-  -F "knn=$KNN" \
   -F "query=@$QUERY_PATH" \
   -F "reference=@$REF_PATH" \
   $SERVER_URL/knn)
 
-#echo "Job has name: $NAME"
+echo "Job has name: $NAME" >&2
 
-curl -q \
-  $SERVER_URL/awaitComplete?name=$NAME
+while true; do
+    curl -q $SERVER_URL/awaitComplete?name=$NAME
+    if [ $? -eq 0 ]; then
+        exit 0
+    fi
+    sleep 5
+done
