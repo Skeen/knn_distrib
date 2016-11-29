75ab0d80f84024c143ec2787726c936d82581fab
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-11-29 18:11:07 +0100

    Request outputs progress and stuff

--- request.sh
+++ request.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#echo Reference: $1
-#echo Query: $2
-
 SERVER_URL=http://localhost:3001
 
 TIMEOUT=100000
@@ -11,17 +8,43 @@ SPLIT=1
 REF_PATH=$1
 QUERY_PATH=$2
 
+# Debug info
+echo "Server: $SERVER_URL" >&2
+echo "Timeout: $TIMEOUT" >&2
+echo "Split: $SPLIT" >&2
+echo "Reference: $REF_PATH" >&2
+echo "Query: $QUERY_PATH" >&2
+echo "" >&2
+
+# Upload task
+echo "Uploading data for the task:" >&2
+
 NAME=$(curl -q \
+  --progress-bar \
   -F "timeout=$TIMEOUT" \
   -F "split=$SPLIT" \
   -F "query=@$QUERY_PATH" \
   -F "reference=@$REF_PATH" \
   $SERVER_URL/knn)
 
-echo "Job has name: $NAME" >&2
+# Print jobname
+echo "" >&2
+echo "Task submitted: Job has name: $NAME" >&2
+echo "" >&2
 
+# Print out progress information
 while true; do
-    curl -q $SERVER_URL/awaitComplete?name=$NAME
+    PROGRESS=$(curl -qs $SERVER_URL/progress?name=$NAME)
+    echo -en "\r\e[0K Progress: $PROGRESS" >&2
+    if [ "$PROGRESS" = "100%" ]; then
+        break
+    fi
+    sleep 5
+done
+
+# Download the result
+while true; do
+    curl -qs $SERVER_URL/awaitComplete?name=$NAME
     if [ $? -eq 0 ]; then
         exit 0
     fi
